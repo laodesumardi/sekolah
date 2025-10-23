43e9c768b1d9e8bdc3d8e0077e931ba3110983dc
laodesumardi <sumardilaode04@gmail.com>
2025-10-23 19:08:19 +0900

    feat(storage): tambah symlink public/profil ke storage/app/public/profil dan siapkan folder target

--- config/filesystems.php
+++ config/filesystems.php
@@ -76,6 +76,7 @@ return [
     'links' => [
         public_path('storage') => storage_path('app/public'),
         public_path('perpustakaan') => storage_path('app/public/perpustakaan'),
+        public_path('profil') => storage_path('app/public/profil'),
     ],
 
 ];
